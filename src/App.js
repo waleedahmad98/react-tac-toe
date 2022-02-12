3fb97fd976157fc503993e296b8dc9b26a28c247
waleedahmad98 <w.ahmad1024@gmail.com>
2022-02-13 00:51:10 +0500

    beta 2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Board from './components/Board';
+import { useState } from 'react';
 
 function App() {
+  const [status, setStatus] = useState("")
+
+  const player = 'X';
+  const opponent = 'O';
+
+  let winner = "";
+  if (status === player){
+    winner = <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 64 64"><path fill="#ff5a79" d="M62 10.6L53.4 2L32 23.4L10.6 2L2 10.6L23.4 32L2 53.4l8.6 8.6L32 40.6L53.4 62l8.6-8.6L40.6 32z"/></svg>
+  }
+  else if (status === opponent){
+    winner = <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 64 64"><path d="M32 2C15.4 2 2 15.4 2 32s13.4 30 30 30s30-13.4 30-30S48.6 2 32 2m0 51c-11.6 0-21-9.4-21-21s9.4-21 21-21s21 9.4 21 21s-9.4 21-21 21" fill="#ff5a79"/></svg>
+  }
+
   return (
-    <div className='container d-flex justify-content-around align-items-center h-100'>
-      <Board/>
+    <div className='container d-flex flex-column justify-content-around align-items-center h-100'>
+      <Board  player = {player} opponent = {opponent} setStatus = {setStatus}/>
+      <h1 className='h-25'>{winner}</h1>
     </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,53 +1,161 @@
-import React, { useState } from 'react';
-import Box from './Box';
-import findBestMove from '../logic/minimax';
+import React, { useState } from "react";
+import Box from "./Box";
+import findBestMove from "../logic/minimax";
 
+export default function Board(props) {
+    const [board, setBoard] = useState([
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+    ]);
 
+    const checkBoxesForWin = (r1, c1, r2, c2, r3, c3, symbol) => {
+        if (board[r1][c1] === board[r2][c2] && board[r2][c2] === board[r3][c3] && board[r3][c3] === symbol)
+            return true;
+        return false;
+    };
 
-export default function Board() {
-    const [board, setBoard] = useState([['', '', ''], ['', '', ''], ['', '', '']])
+    const drawWin = (r1, c1, r2, c2, r3, c3, symbol) => {
+        let copyboard = [...board];
+        copyboard[r1][c1] = symbol + "W";
+        copyboard[r2][c2] = symbol + "W";
+        copyboard[r3][c3] = symbol + "W";
+        setBoard(copyboard);
+    }
 
+    const checkWin = (symbol) => {
+        if (checkBoxesForWin(0, 0, 0, 1, 0, 2, symbol) === true) {
+            drawWin(0, 0, 0, 1, 0, 2, symbol)
+            return true;
+        }
+        else if (checkBoxesForWin(1, 0, 1, 1, 1, 2, symbol) === true) {
+            drawWin(1, 0, 1, 1, 1, 2, symbol)
+            return true;
+        }
+        else if (checkBoxesForWin(2, 0, 2, 1, 2, 2, symbol) === true) {
+            drawWin(2, 0, 2, 1, 2, 2, symbol)
+            return true;
+        }
+        else if (checkBoxesForWin(0, 0, 1, 0, 2, 0, symbol) === true) {
+            drawWin(0, 0, 1, 0, 2, 0, symbol)
+            return true;
+        }
+        else if (checkBoxesForWin(0, 1, 1, 1, 2, 1, symbol) === true) {
+            drawWin(0, 1, 1, 1, 2, 1, symbol)
+            return true;
+        }
+        else if (checkBoxesForWin(0, 2, 1, 2, 2, 2, symbol) === true) {
+            drawWin(0, 2, 1, 2, 2, 2, symbol)
+            return true;
+        }
+        else if (checkBoxesForWin(0, 0, 1, 1, 2, 2, symbol) === true) {
+            drawWin(0, 0, 1, 1, 2, 2, symbol)
+            return true;
+        }
+        else if (checkBoxesForWin(0, 2, 1, 1, 2, 0, symbol) === true) {
+            drawWin(0, 2, 1, 1, 2, 0, symbol)
+            return true;
+        }
+    };
     const makeMove = () => {
         let bestMove = findBestMove(board);
         let copyboard = [...board];
-        copyboard[bestMove.row][bestMove.col] = 'O'
-        setBoard(copyboard)
-    }
+        copyboard[bestMove.row][bestMove.col] = props.opponent;
+        setBoard(copyboard);
+        if (checkWin(props.player) === true)
+            props.setStatus(props.player)
+        if (checkWin(props.opponent) === true)
+            props.setStatus(props.opponent)
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j<3; j++){
+                if (board[i][j] === '')
+                    return
+            }
+        }
+        props.setStatus('D');
+        
+    };
 
     return (
         <div>
-            <div className='row'>
-                <div className='col-4 tl'>
-                    <Box board={board} setBoard={setBoard} place={{row: 0, col: 0}} makeMove = {makeMove} />
+            <div className="row">
+                <div className="col-4 tl">
+                    <Box
+                        board={board}
+                        setBoard={setBoard}
+                        place={{ row: 0, col: 0 }}
+                        makeMove={makeMove}
+                    />
                 </div>
-                <div className='col-4 tm'>
-                    <Box board={board} setBoard={setBoard} place={{row: 0, col: 1}} makeMove = {makeMove}/>
+                <div className="col-4 tm">
+                    <Box
+                        board={board}
+                        setBoard={setBoard}
+                        place={{ row: 0, col: 1 }}
+                        makeMove={makeMove}
+                    />
                 </div>
-                <div className='col-4 tr'>
-                    <Box board={board} setBoard={setBoard} place={{row: 0, col: 2}} makeMove = {makeMove}/>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col-4 ml'>
-                    <Box board={board} setBoard={setBoard} place={{row: 1, col: 0}} makeMove = {makeMove}/>
-                </div>
-                <div className='col-4 mm'>
-                    <Box board={board} setBoard={setBoard} place={{row: 1, col: 1}} makeMove = {makeMove}/>
-                </div>
-                <div className='col-4 mr'>
-                    <Box board={board} setBoard={setBoard} place={{row: 1, col: 2}} makeMove = {makeMove}/>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col-4 bl'>
-                    <Box board={board} setBoard={setBoard} place={{row: 2, col: 0}} makeMove = {makeMove}/>
-                </div>
-                <div className='col-4 bm'>
-                    <Box board={board} setBoard={setBoard} place={{row: 2, col: 1}} makeMove = {makeMove}/>
-                </div>
-                <div className='col-4 br'>
-                    <Box board={board} setBoard={setBoard} place={{row: 2, col: 2}} makeMove = {makeMove}/>
+                <div className="col-4 tr">
+                    <Box
+                        board={board}
+                        setBoard={setBoard}
+                        place={{ row: 0, col: 2 }}
+                        makeMove={makeMove}
+                    />
                 </div>
             </div>
-        </div>);
+            <div className="row">
+                <div className="col-4 ml">
+                    <Box
+                        board={board}
+                        setBoard={setBoard}
+                        place={{ row: 1, col: 0 }}
+                        makeMove={makeMove}
+                    />
+                </div>
+                <div className="col-4 mm">
+                    <Box
+                        board={board}
+                        setBoard={setBoard}
+                        place={{ row: 1, col: 1 }}
+                        makeMove={makeMove}
+                    />
+                </div>
+                <div className="col-4 mr">
+                    <Box
+                        board={board}
+                        setBoard={setBoard}
+                        place={{ row: 1, col: 2 }}
+                        makeMove={makeMove}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4 bl">
+                    <Box
+                        board={board}
+                        setBoard={setBoard}
+                        place={{ row: 2, col: 0 }}
+                        makeMove={makeMove}
+                    />
+                </div>
+                <div className="col-4 bm">
+                    <Box
+                        board={board}
+                        setBoard={setBoard}
+                        place={{ row: 2, col: 1 }}
+                        makeMove={makeMove}
+                    />
+                </div>
+                <div className="col-4 br">
+                    <Box
+                        board={board}
+                        setBoard={setBoard}
+                        place={{ row: 2, col: 2 }}
+                        makeMove={makeMove}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
